7d72140cd6566526ceeebc0e9edc1b2c7f34b634
henry <101145848+h-aig@users.noreply.github.com>
2026-03-06 22:28:04 +0000

    catch exception if url is incorrectly formatted

--- aquamarine/Program.vb
+++ aquamarine/Program.vb
@@ -1,23 +1,26 @@
+' (<3 dotnetperls, https://www.dotnetperls.com/webclient-vbnet)
 Imports System
 Imports System.Net
 
 Module Program
     Sub Main(args As String())
+        dim webpagepretty as String
+        dim client as new WebClient
+        Dim webpageraw As string
+        client.Headers("User-Agent") = "Mozilla/4.0" ' Set one of the headers.        
+        
         Console.WriteLine("aquamarine, a typing test for your favorite blog posts")
         Console.WriteLine()
         
-        ' Resource acquisition statement. (<3 dotnetperls, https://www.dotnetperls.com/webclient-vbnet example 3)
-        Using client As New WebClient
-            ' Set one of the headers.
-            client.Headers("User-Agent") = "Mozilla/4.0"
-
-            ' Download data as string
-            Dim value As string = client.DownloadString("http://www.example.com/")
-
-            ' Display result.
-            Console.WriteLine(value)
-        End Using
+        try
+        webpageraw = client.DownloadString("http://www.example.com/") ' Download data as string
+            Catch ex as Exception ' if the url is incorrect
+                webpageraw = "There was an exception!" ' EXPAND ON THIS: WHAT TO DO IF THE URL IS INCORRECT?
+            End Try
+        
+        Console.WriteLine((webpageraw))
         
         Console.ReadLine()
     End Sub
+    
 End Module

d1aa4c435b9f8c8cd5c54a634366c0b2bc89f946
henry <101145848+h-aig@users.noreply.github.com>
2026-03-06 21:54:02 +0000

    add webpage download framework

--- aquamarine/Program.vb
+++ aquamarine/Program.vb
@@ -1,7 +1,23 @@
 Imports System
+Imports System.Net
 
 Module Program
     Sub Main(args As String())
-        Console.WriteLine("Hello World!")
+        Console.WriteLine("aquamarine, a typing test for your favorite blog posts")
+        Console.WriteLine()
+        
+        ' Resource acquisition statement. (<3 dotnetperls, https://www.dotnetperls.com/webclient-vbnet example 3)
+        Using client As New WebClient
+            ' Set one of the headers.
+            client.Headers("User-Agent") = "Mozilla/4.0"
+
+            ' Download data as string
+            Dim value As string = client.DownloadString("http://www.example.com/")
+
+            ' Display result.
+            Console.WriteLine(value)
+        End Using
+        
+        Console.ReadLine()
     End Sub
 End Module

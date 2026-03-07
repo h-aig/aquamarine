44db265c1a75bf91ea715511af2e22a826345ac7
henry <101145848+h-aig@users.noreply.github.com>
2026-03-07 08:20:37 +0000

    create nodelist

--- aquamarine/Program.vb
+++ aquamarine/Program.vb
@@ -1,24 +1,28 @@
 ' (<3 dotnetperls, https://www.dotnetperls.com/webclient-vbnet)
 Imports System
 Imports System.Net
+Imports System.Xml
 
 Module Program
+    
     Sub Main(args As String())
-        dim webpagepretty as String
         dim client as new WebClient
-        Dim webpageraw As string
+        Dim rawRSS As string
+        dim prettyRSS as new xmldocument()
         client.Headers("User-Agent") = "Mozilla/4.0" ' Set one of the headers.        
         
         Console.WriteLine("aquamarine, a typing test for your favorite blog posts")
         Console.WriteLine()
         
         try
-        webpageraw = client.DownloadString("http://www.example.com/") ' Download data as string
+        rawRSS = client.DownloadString("https://feeds.bbci.co.uk/news/england/rss.xml") ' Download data as string
             Catch ex as Exception ' if the url is incorrect
-                webpageraw = "There was an exception!" ' EXPAND ON THIS: WHAT TO DO IF THE URL IS INCORRECT?
-            End Try
+                rawRSS = "There was an exception!" ' EXPAND ON THIS: WHAT TO DO IF THE URL IS INCORRECT?
+            End try
+        prettyRSS.load(rawRSS)
         
-        Console.WriteLine((webpageraw))
+        Dim nodeList as XmlNodeList 
+        Dim root as XmlNode = prettyRSS.DocumentElement
         
         Console.ReadLine()
     End Sub

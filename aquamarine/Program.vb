60f673d6a703fb9e2170e98afe03512e2c957b47
henry <101145848+h-aig@users.noreply.github.com>
2026-03-07 14:14:32 +0000

    add select case for menu nav -- block character input

--- aquamarine/Program.vb
+++ aquamarine/Program.vb
@@ -4,15 +4,15 @@ Imports System.Net
 Imports System.Xml
 
 Module Program
-    
     Sub Main(args As String())
+        Console.Clear() ' get rid of the accursed yellow boot text in rider-- no effect in prod
         dim rssList(0) as String ' stores list of all rss posts
         dim client as new WebClient ' client
         Dim rawRSS As string ' this stores the downloaded string
         dim xmlRSS as new xmldocument() ' this holds the downloaded string as an xmldocument
-        client.Headers("User-Agent") = "Mozilla/4.0" ' Set one of the headers.        
+        client.Headers("User-Agent") = "Mozilla/4.0" ' Set one of the headers.   
         
-        Console.WriteLine("aquamarine, a typing test for your favorite blog posts")
+        Console.WriteLine("Aquamarine. A new way to consume information.")
         Console.WriteLine()
         
         try
@@ -25,18 +25,42 @@ Module Program
         dim item as XmlNode
         Dim nodeList as XmlNodeList ' i need a try here-- if the xml is invalid
         Dim root as XmlNode = xmlRSS.DocumentElement
-        
         nodeList=root.SelectNodes("/rss/channel/item")
         
-        for each item in nodeList
-            dim title as XmlNode = item.SelectSingleNode("title")
-            'Console.WriteLine(title.InnerText)
-            Array.Resize(rssList, rssList.Length+1)
-            rssList(rssList.Length-1) = title.InnerText
+        for each item in nodeList ' for each post in the feed...
+            dim title as XmlNode = item.SelectSingleNode("title") ' find post
+            rssList(rssList.Length-1) = title.InnerText ' place post in rssList
+            Array.Resize(rssList, rssList.Length+1) ' increase size of array by 1
         next 
+        Array.resize(rssList, rsslist.length-1) ' get rid of empty string at the end
         for i as integer = 0 to rssList.Length-1
             console.WriteLine(rssList(i))
         Next
+        
+        Console.SetCursorPosition(0,2)
+        Console.BackgroundColor = ConsoleColor.White
+        Console.ForegroundColor = ConsoleColor.Black
+        Console.Write(rssList(0))
+            
+        While True
+            Dim key As ConsoleKeyInfo = Console.ReadKey(True) ' True = don't display
+            Select Case key.Key ' Merge all keys into one Case -- https://chatgpt.com/s/t_69ac323c5adc8191ac526fa15f7b7065
+                Case ConsoleKey.UpArrow, ConsoleKey.DownArrow, ConsoleKey.LeftArrow, ConsoleKey.RightArrow, ConsoleKey.Enter
+                    ' Handle each key individually
+                    If key.Key = ConsoleKey.UpArrow Then
+                        Console.WriteLine("Up pressed")
+                    ElseIf key.Key = ConsoleKey.DownArrow Then
+                        Console.WriteLine("Down pressed")
+                    ElseIf key.Key = ConsoleKey.LeftArrow Then
+                        Console.WriteLine("Left pressed")
+                    ElseIf key.Key = ConsoleKey.RightArrow Then
+                        Console.WriteLine("Right pressed")
+                    ElseIf key.Key = ConsoleKey.Enter Then
+                        Console.WriteLine("Enter pressed")
+                        Exit While
+                    End If
+            End Select
+        End While
         
         Console.ReadLine()
     End Sub

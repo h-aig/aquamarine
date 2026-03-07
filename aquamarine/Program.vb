42179030ac8a3932333d0ee3bfd41d850cf4251c
henry <101145848+h-aig@users.noreply.github.com>
2026-03-07 16:37:22 +0000

    add menu system

--- aquamarine/Program.vb
+++ aquamarine/Program.vb
@@ -6,6 +6,7 @@ Imports System.Xml
 Module Program
     Sub Main(args As String())
         Console.Clear() ' get rid of the accursed yellow boot text in rider-- no effect in prod
+        Console.ForegroundColor = ConsoleColor.White
         dim rssList(0) as String ' stores list of all rss posts
         dim client as new WebClient ' client
         Dim rawRSS As string ' this stores the downloaded string
@@ -45,16 +46,30 @@ Module Program
         While True
             Dim key As ConsoleKeyInfo = Console.ReadKey(True) ' True = don't display
             Select Case key.Key ' Merge all keys into one Case -- https://chatgpt.com/s/t_69ac323c5adc8191ac526fa15f7b7065
-                Case ConsoleKey.UpArrow, ConsoleKey.DownArrow, ConsoleKey.LeftArrow, ConsoleKey.RightArrow, ConsoleKey.Enter
+                Case ConsoleKey.UpArrow, ConsoleKey.DownArrow, ConsoleKey.Enter
                     ' Handle each key individually
                     If key.Key = ConsoleKey.UpArrow Then
-                        Console.WriteLine("Up pressed")
+                        if not Console.CursorTop = 2
+                            console.cursorleft = 0
+                            Console.BackgroundColor = ConsoleColor.Black
+                            Console.ForegroundColor = ConsoleColor.white
+                            Console.Write(rssList(Console.CursorTop - 2))
+                            console.SetCursorPosition(0, console.CursorTop - 1)
+                            Console.BackgroundColor = ConsoleColor.White
+                            Console.ForegroundColor = ConsoleColor.Black
+                            Console.Write(rssList(Console.CursorTop - 2))
+                        End If
                     ElseIf key.Key = ConsoleKey.DownArrow Then
-                        Console.WriteLine("Down pressed")
-                    ElseIf key.Key = ConsoleKey.LeftArrow Then
-                        Console.WriteLine("Left pressed")
-                    ElseIf key.Key = ConsoleKey.RightArrow Then
-                        Console.WriteLine("Right pressed")
+                        if not Console.CursorTop = rssList.Length + 1 'array starts at 0, so it's +1 and not +2 in terms of boundary checking
+                            console.CursorLeft = 0
+                            Console.BackgroundColor = ConsoleColor.black
+                            Console.ForegroundColor = ConsoleColor.white
+                            Console.Write(rssList(Console.CursorTop - 2))
+                            console.SetCursorPosition(0, console.CursorTop + 1)
+                            Console.BackgroundColor = ConsoleColor.White
+                            Console.ForegroundColor = ConsoleColor.Black
+                            Console.Write(rssList(Console.CursorTop - 2))
+                        end if
                     ElseIf key.Key = ConsoleKey.Enter Then
                         Console.WriteLine("Enter pressed")
                         Exit While

fc213e16010480354075dffc6b7ac8b824c33ffd
h <101145848+h-aig@users.noreply.github.com>
2026-07-06 11:33:36 +0100

    add getNodes to get titles and links

--- aquamarine/Program.vb
+++ aquamarine/Program.vb
@@ -1,25 +1,16 @@
-Imports System
 Imports System.Net
-Imports System.Net.Http
-Imports System.Net.Http.Headers
-Imports System.Net.Mime
-Imports System.Runtime.Intrinsics.X86
-Imports System.Security.Cryptography
-Imports System.Xml
-Imports AngleSharp.Html.Dom
 Imports System.ServiceModel.Syndication
 Imports System.Text
-Imports SmartReader
+Imports System.Xml
 
 Module Program
+    Private _nodeList as XmlNodeList
+    Private _titlesAndLinks (,) as String ' create 2d array for storing titles/links
+    
     sub Main()
         Console.Clear()
-        dim feedUrl as string = getFeed()
-        dim synFeed as SyndicationFeed
-        
-        Using reader as XmlReader = xmlreader.Create(feedUrl)
-            synFeed = SyndicationFeed.Load(reader)
-        End Using
+        dim feedUrl as string = getFeed() ' create feed url with verification
+        GetNodes(feedUrl)
         
         
     End sub
@@ -27,7 +18,7 @@ Module Program
     Function GetFeed()
         dim client as new WebClient
         client.Headers("User-Agent") = "Mozilla/4.0"
-        
+
         Console.Write("Link to feed: ")
         dim submittedFeed as string = console.ReadLine()
         dim feedValid as Boolean
@@ -44,8 +35,46 @@ Module Program
                 submittedFeed = console.Readline()
             End Try
         loop
-        
+
         return submittedFeed
-        
     End Function
+    
+    sub GetNodes(feedUrl)
+        dim synFeed as SyndicationFeed
+        
+        Using reader as XmlReader = xmlreader.Create(feedUrl)
+            synFeed = SyndicationFeed.Load(reader) ' make feed or something
+        End Using
+        
+        dim sb as new StringBuilder()
+        using writer as XmlWriter = XmlWriter.Create(sb)
+            synFeed.SaveAsRss20(writer)
+        End Using
+        
+        dim xmlRss as new XmlDocument()
+        xmlRss.LoadXml(sb.ToString())
+
+        dim root as XmlLinkedNode = xmlRss.DocumentElement
+        _nodeList = root.SelectNodes("//*[local-name()='entry' or local-name()='item']")
+        
+        dim item as XmlNode
+        dim itemsInNodeList as Integer = 0
+        for each item in _nodeList
+            itemsInNodeList += 1 
+        next
+        itemsInNodeList -= 1
+        
+        ReDim _titlesAndLinks(itemsInNodeList, itemsInNodeList) ' resize array to the number of links/stories
+        
+        for i as integer = 0 to itemsInNodeList
+            dim title as XmlNode = item.SelectSingleNode("title")
+            _titlesAndLinks(i, 0) = title.InnerText
+            dim link as XmlNode = item.SelectSingleNode("link")
+            _titlesAndLinks(i, 1) = link.InnerText
+        Next ' grab all titles and links
+        
+        
+        
+    End sub 
+    
 End Module
